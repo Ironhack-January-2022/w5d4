0e214a18b77c92871d4971e669606ccdfb1c75e5
Jan Redmann <janredmann1@gmail.com>
2022-02-10 15:07:58 +0100

    add chart

--- chart-js/script.js
+++ chart-js/script.js
@@ -8,9 +8,29 @@ const printChart = data => {
 	const stockDates = Object.keys(dailyData)
 	console.log(stockDates)
 	const stockPrices = stockDates.map(date => {
-		return dailyData[date]['4.close']
+		return dailyData[date]['4. close']
 	})
 	console.log(stockPrices)
+
+	// drawing the chart
+	const ctx = document.querySelector('#myChart').getContext('2d')
+
+	new Chart(ctx, {
+		type: 'line',
+		data: {
+			// x - axis:
+			labels: stockDates,
+			datasets: [
+				{
+					label: 'Stock Chart',
+					backGroundColor: 'rgb(255, 99, 132)',
+					borderColor: 'rgb(255, 99, 132)',
+					// y - axis: 
+					data: stockPrices
+				}
+			]
+		}
+	})
 }
 
 fetch(apiUrl)
